912ec7868ef87616ae93d80c2cce66e555d7c4e4
Levi <levgoolie@gmail.com>
2025-12-18 21:07:56 +1100

    Update colorbot.py

--- colorbot.py
+++ colorbot.py
@@ -18,7 +18,7 @@ try:
 
  mouse_event = ctypes.windll.user32.mouse_event
  print("""
-     \033[1;35m v21 """)
+     \033[1;35m v22 """)
  print("\n\033[0m[\033[1;34mINFO\033[0m] Loaded")
 
  print("\033[1;36m[\033[0;36m1\033[0m] 160x160")
